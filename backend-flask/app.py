471987bec430c72c21443afd4e3504719f497aa8
teriqTT <teriqtrimtt@gmail.com>
2023-03-04 14:46:49 +0000

    App.py updates and docker compose,observability

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,7 +14,29 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+#honeycomb......
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+#honeycomb..
+#initalize tracing and an exporter than can send data to honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
 app = Flask(__name__)
+#Honeycomb
+#Initliaze automatic instrumentation with flask
+
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
